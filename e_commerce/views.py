3f19fb2f8ec34b356be70c30317d48de9e8eec13
ThalesHenri <thales1337@hotmail.com>
2024-01-17 13:13:36 -0300

    create and integrate about, contact

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
-    return render(request, "index.html", {})
+    context = {
+            "title": "pagina principal",
+            "content": "Bem-Vindo a página principal"
+    }
+    return render(request, "index.html",context)
+
+def about_page(request):    
+    context = {
+        "title": "pagina sobre",
+        "content": "bem vindo a pagina sobre"
+    }
+    return render(request, "index.html",context)
+def contact_page(request): 
+    context = {
+        "title": "pagina CONTATO",
+        "content": "bem vindo a pagina Contatos"
+    }
+    return render(request, "index.html",context)
